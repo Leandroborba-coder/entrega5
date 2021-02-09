eb355b0e28f9d07550f04ebabc75dc754ebbf37c
Leandroborba-coder <leandroborba@poli.ufrj.br>
2021-02-09 20:34:08 -0300

    entrega5

--- src/components/Down/index.js
+++ src/components/Down/index.js
@@ -1,15 +1,25 @@
 import React from 'react';
 import './styles.css';
 import logoheader from '../Assets/logoheader.png';
+import redes from '../Assets/redes.PNG';
 
 function Down(){
     return(
     <div className="princ">
 
         <div className="downlogo">
-            <h2>ioGO!</h2> 
+            <div className="downlogo-marca">
+                <h2>ioGO!</h2>
+            </div>
+
+            <div className="downlogo-foto">   
+                <img src={redes} height="40em" />
+            </div>
+            
+            
             
         </div>
+
 
         <div className="rigths">
             <h5>2021 / ioGO! / Todos os direitos reservados</h5>
